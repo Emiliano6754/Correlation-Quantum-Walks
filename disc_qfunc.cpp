4eabf6fef7085f6f4d8852b79e83b795c47f15ae
Emiliano Pastrana <emilianopm93@gmail.com>
2024-06-19 14:07:29 -0600

    Changed pow for std version for optimization

--- disc_qfunc.cpp
+++ disc_qfunc.cpp
@@ -1,6 +1,7 @@
 #include "disc_qfunc.h"
 #include<map>
 #include<bit>
+#include<cmath>
 
 // Calculates the field-wise trace of alpha by calculating its hamming weight and returning the last bit (modulo 2)
 inline unsigned int trace(unsigned int alpha) {
@@ -16,7 +17,7 @@ inline unsigned int trace(unsigned int alpha, unsigned int beta) {
 inline void generate_xi_buffer(std::map<unsigned int, std::complex<double>> &xi_buffer, const unsigned int &n_qubits, const std::complex<double> &xi = 0.5*(sqrt(3)-1)*std::complex<double>(1.0,1.0)) {
     std::complex<double> xi_conj = std::conj(xi);
     for (unsigned int n = 0; n < n_qubits; n++) {
-        xi_buffer[n] = pow(xi_conj,n);
+        xi_buffer[n] = std::pow(xi_conj,n);
     }
 }
 
@@ -24,7 +25,7 @@ inline void sym_Qfunc(Eigen::MatrixXcd &Qfunc, const unsigned int &n_qubits, con
 
 }
 
-// Returns the sum of Q^2 for state in sum. Supports max 32 qubits, limited by qubitstate_size and the variables for the loops
+// Returns the sum of Q^2 for state in sum. Requires state to be normalized. Else, it returns norm^2 * sum Q^2. Supports max 32 qubits, limited by qubitstate_size and the variables for the loops
 inline void sym_sumQ2(double &sum, const unsigned int &n_qubits, const Eigen::VectorXcd &state) {
     const unsigned int qubitstate_size = 1 << n_qubits;
     const std::complex<double> xi = 0.5*(sqrt(3)-1)*std::complex<double>(1.0,1.0);
@@ -32,7 +33,7 @@ inline void sym_sumQ2(double &sum, const unsigned int &n_qubits, const Eigen::Ve
     generate_xi_buffer(xi_buffer,n_qubits,xi);
     std::map<unsigned int, std::complex<double>> im_buffer{{0,std::complex<double>(1.0,0.0)},{1,std::complex<double>(0.0,-1.0)}}; // Required for (-i)^tr(ab)
     std::map<unsigned int, double> sign_buffer{{0,1.0},{1,-1.0}}; // Required for (-1)^tr(ab). std::complex requires double to perform multiplication :|
-    const double denom = 1.0 / pow(1+std::norm(xi), n_qubits);
+    const double denom = 1.0 / std::pow(1+std::norm(xi), n_qubits);
     std::complex<double> coeff;
     for (unsigned int alpha = 0; alpha < qubitstate_size; alpha++) {
         for (unsigned int beta = 0; beta < qubitstate_size; beta++) {
@@ -41,7 +42,7 @@ inline void sym_sumQ2(double &sum, const unsigned int &n_qubits, const Eigen::Ve
                 coeff += sign_buffer[trace(alpha,eta)] * xi_buffer[std::popcount(beta ^ eta)] * state[eta];
             }
             coeff = im_buffer[trace(alpha,beta)] * coeff;
-            sum += std::norm(coeff);
+            sum += std::pow(std::norm(coeff),2);
         }
     }
     sum *= denom;
